f8bd19d7027800bbba2ea978df22e2d125d5e48e
HannoSpreeuw <h.spreeuw@esciencecenter.nl>
2020-07-13 16:58:03 +0200

    This script now actually does what its name suggests. Uncorrelated Gaussian noise with a std of 1 Jy. Peaks of 167000 inserted sources on a regular grid at about 8.3 Jy. PySE takes forever to process this, so we need to assert if we want to optimize PySE for maps with such high source densities.

--- make_4k_by_4k_map_with_Gaussian_noise_and_167000_sources_on_a_regular_grid.py
+++ make_4k_by_4k_map_with_Gaussian_noise_and_167000_sources_on_a_regular_grid.py
@@ -1,76 +1,56 @@
 # This version of test_deblending is much simpler than the original one, because the flux of the weakest source is 
 # the same in all ~10000 maps.
 import numpy as np
+import scipy.stats as st
 from astropy.io import fits
+from scipy import signal
 import pylab as pyl
-import os
+import os, math
 
 input_fits = '89M18BIGPBC.FITS'
 output_fits = 'SOURCESINSERTED.FITS'
 
-degtorad = np.pi / 180.0
-raincr_rad = -3.333333414E-03 * degtorad
-decincr_rad = 3.333333414E-03 * degtorad
-alpha0 = 2.66363244382E+02 * degtorad
-delta0 = -2.99529359725E+01 * degtorad
-
-
-# # Make a series of Gaussians with separations linearly increasing.
-# # We'll insert those in the maps. They are separated perpendicular to the major axis, so along the minor axis.
-# number_of_separation_steps=20
-# gaussians=np.zeros((number_of_separation_steps+1,mapsize,mapsize),'d')
-# centrex=np.zeros((number_of_separation_steps+1),'d')
-# centrey=np.zeros((number_of_separation_steps+1),'d')
-# pixelsize=12.
-# theta=-np.pi*49.80/180.
-# semimajor=0.5*67.1472/pixelsize
-# semiminor=0.5*56.1528/pixelsize
-# 
-# minor_width=semiminor/math.sqrt(2.*math.log(2.))
-# print 'minor_width=',minor_width
-# beginsep=2.0*minor_width
-# endsep=7.0*minor_width
-# 
-# j=0
-# while j<=number_of_separation_steps:
-#     # I conveniently use sign(j) because sign(0)=0 and sign(1,2,3..)=1.
-#     centrex[j]=centrexstart+np.sign(j)*np.cos(theta)*(beginsep+float(j-1)*(endsep-beginsep)/float(number_of_separation_steps-1))
-#     centrey[j]=centreystart+np.sign(j)*np.sin(theta)*(beginsep+float(j-1)*(endsep-beginsep)/float(number_of_separation_steps-1))
-#     print 'this is j and the separation:',j,np.sign(j)*(beginsep+float(j-1)*(endsep-beginsep)/float(number_of_separation_steps-1))
-#     k=0
-#     while k<mapsize:
-#         x=float(k)
-#         l=0
-#         while l<mapsize:
-#             y=float(l)
-#             gaussians[j,k,l]=np.exp(-math.log(2.)*(((np.cos(theta)*(x-centrex[j])+np.sin(theta)*(y-centrey[j]))/semiminor)**2.+((np.cos(theta)*(y-centrey[j])-np.sin(theta)*(x-centrex[j]))/semimajor)**2.))
-#             l+=1
-#         k+=1
-#     j+=1
-
+number_of_sources = 167000
 
 hdulist=fits.open(input_fits)
 dimensions = (hdulist[0].header['NAXIS1'], hdulist[0].header['NAXIS2'])
 print
 print "The dimensions of this image are: ",dimensions
 
-sources=np.zeros(dimensions,'d')
+all_sources_expanded_to_image_size=np.zeros(dimensions,'d')
 
-# q=0
-# # print 'this is the separation index:',1+math.fmod(i-1,number_of_separation_steps)
-# while q<8:
-#     p=0
-#     while p<8:
-#         xstart=mapsize*q
-#         x=xstart+edgeskip
-#         ystart=mapsize*p
-#         y=ystart+edgeskip
-#         sources[x-dx:x+dx,y-dy:y+dy]+=gaussians[0,:,:]*inserted_fluxes[i-1]
-#         p+=1
-#     q+=1
+number_of_sources_along_equal_axes = int(math.ceil(np.sqrt(number_of_sources)))
+number_of_sources_along_xaxis = int(number_of_sources_along_equal_axes*dimensions[0]/dimensions[1])
+number_of_sources_along_yaxis = int(number_of_sources_along_equal_axes*dimensions[1]/dimensions[0])
 
-# This gives us random noise, normally distributed, as single precision floats.
-hdulist['PRIMARY'].data = np.float32(np.random.normal(size=dimensions))
+print
+print "number_of_sources_along_xaxis = ",number_of_sources_along_xaxis,"number_of_sources_along_yaxis = ",number_of_sources_along_yaxis
+
+kernlenx = int(dimensions[0]/number_of_sources_along_xaxis)
+kernleny = int(dimensions[1]/number_of_sources_along_yaxis)
+print
+print "kernlenx = ",kernlenx,"  kernleny = ",kernleny
+
+# Here I try to make enough copies of a 2D asymmetrical Gaussian to cover the entire map, with peaks well above the noise
+def gkern_asym(kernlenx, kernleny, peak=10, stdx=1, stdy=1.5):
+    """Returns a 2D Gaussian kernel array."""
+    gkernx = signal.gaussian(kernlenx, std=stdx)
+    gkerny = signal.gaussian(kernleny, std=stdy)
+    gkern2d = np.outer(gkernx, gkerny)
+    return peak*gkern2d
+
+asym_gauss = gkern_asym(kernlenx, kernleny)
+all_sources=np.tile(asym_gauss,(number_of_sources_along_xaxis, number_of_sources_along_yaxis))
+
+edgex = int((dimensions[0]-all_sources.shape[0])/2)
+edgey = int((dimensions[1]-all_sources.shape[1])/2)
+print
+print "edgex = ",edgex,"  edgey = ",edgey
+
+all_sources_expanded_to_image_size[edgex:edgex+all_sources.shape[0], edgey:edgey+all_sources.shape[1]] = all_sources
+
+# This gives us random noise, normally distributed, as single precision floats with strong sources on a regular grid added to that.
+hdulist['PRIMARY'].data = np.float32(np.random.normal(size=dimensions))+all_sources_expanded_to_image_size
 scidata=hdulist['PRIMARY'].data
 # These should be exact copies of the source free maps!!!
 try: 
@@ -82,44 +62,3 @@ except IOError:
 
 hdulist.close()
  
-#  # The file is opened immediately after it has been made.
-#  my_fitsfile = access.FitsFile(pathout+'SOURCESINSERTED'+str(i)+'.FITS')
-#  my_image= imag.ImageData(my_fitsfile)
-#  # For the source extraction, both the detection and the analysis threshold are set equal to lowerlim in settings.py.
-#  sextract_results = my_image.sextract()
-# 
-#  if len(sextract_results)!=64:
-#      print 'len(sextract_results)=',len(sextract_results)
-#  else:
-#      # Only leap to the next image if there were exactly 64 detections.
-#      ra_values=np.zeros((64),'d')
-#      dec_values=np.zeros((64),'d')
-#      k=0
-#      for detection in sextract_results:
-#          measured_fluxes[i-1,k]=detection.peak.value
-#          reported_flux_errors[i-1,k]=detection.peak.error
-#          ra_values[k]=detection.ra.value
-#          dec_values[k]=detection.dec.value
-#          ra_err[i-1,k]=detection.ra.error
-#          dec_err[i-1,k]=detection.dec.error
-#          # print 'detection.ra,detection.dec=',detection.ra,detection.dec
-#          k+=1
-#      ra_values,dec_values=order_positions(ra_values,dec_values)
-#      # print 'ra_values=',ra_values
-#      # print 'ra_true=',ra_true
-#      ra_diff[i-1,:]=ra_values-ra_true
-#      dec_diff[i-1,:]=dec_values-dec_true
-#      print 'np.std(ra_diff[i-1,:]),np.std(dec_diff[i-1,:])=',np.std(ra_diff[i-1,:]),np.std(dec_diff[i-1,:])
-#  number_not_64+=len(sextract_results)
-#  print 'number_not_64,np.mod(number_not_64,64)=',number_not_64,np.mod(number_not_64,64)
-#  i+=1
-# 
-# np.save(outputpath+'inserted_fluxes',inserted_fluxes)
-# np.save(outputpath+'measured_fluxes',measured_fluxes)
-# np.save(outputpath+'reported_flux_errors',reported_flux_errors)
-# np.save(outputpath+'number_of_high_pixels',number_of_high_pixels)
-# np.save(outputpath+'ra_diff',ra_diff)
-# np.save(outputpath+'ra_err',ra_err)
-# np.save(outputpath+'dec_diff',dec_diff)
-# np.save(outputpath+'dec_err',dec_err)
-
